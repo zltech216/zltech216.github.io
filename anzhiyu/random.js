f8887293f1cd52fbd16536d2c418d602d8e7a715
zhangyuea <1198855231@qq.com>
2025-05-02 11:06:24 +0800

    Site updated: 2025-05-02 11:06:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/01/git命令的常用操作/","2025/05/01/ip归属地的获取和使用/","2025/04/30/mapstruct的使用/","2025/05/01/游标翻页/"];function toRandomPost(){
+var posts=["2025/05/02/MCP框架/","2025/05/01/git命令的常用操作/","2025/05/01/ip归属地的获取和使用/","2025/04/30/mapstruct的使用/","2025/05/01/游标翻页/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
